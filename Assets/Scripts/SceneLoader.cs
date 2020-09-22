f9de3c55846d0bcb9a633b5a173d4ae5892aac9c
Takuto1127 <tiharu717@gmail.com>
2020-09-22 15:07:08 +0900

    Unity Github

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private static int StartSceneIndex = 0;
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene((StartSceneIndex));
+
+    }
+}
